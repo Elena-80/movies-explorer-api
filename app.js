ea8270cb1401aa432d9f284798e3298e13ecaf2d
Elena-80 <elena.chumakova80@yandex.ru>
2023-09-12 15:56:44 +0300

    cors added

---
+++ app.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const {
+  PORT,
+  NODE_ENV,
+  MONGO_URL,
+  MONGO_URL_DEV,
+} = require('./utils/constants');
+const router = require('./routes');
+const cors = require('./middlewares/cors');
+const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const app = express();
+
+mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : MONGO_URL_DEV, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors);
+
+app.use(requestLogger);
+
+app.use(cookieParser());
+app.use(helmet());
+app.use(router);
+
+app.use(errorLogger);
+
+app.use(errors());
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
