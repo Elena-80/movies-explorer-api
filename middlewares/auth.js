8c4529735fe9357fc3ee4443b7213982080856b9
Elena-80 <elena.chumakova80@yandex.ru>
2023-09-30 11:05:50 +0300

    cookie-parser deleted

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,11 +3,31 @@ const { UnauthorizedError } = require('../errors/UnauthorizedError');
 
 const { NODE_ENV, JWT_SECRET, JWT_SECRET_DEV } = require('../config');
 
+// module.exports = (req, res, next) => {
+//   const token = req.cookies.jwt;
+//   if (!token) {
+//     throw new UnauthorizedError('Необходима авторизация.');
+//   }
+//   let payload;
+
+//   try {
+//     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : JWT_SECRET_DEV);
+//   } catch (err) {
+//     throw new UnauthorizedError('Необходима авторизация.');
+//   }
+
+//   req.user = payload;
+//   next();
+// };
+
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (!token) {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new UnauthorizedError('Необходима авторизация.');
   }
+
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
@@ -17,5 +37,6 @@ module.exports = (req, res, next) => {
   }
 
   req.user = payload;
+
   next();
 };
